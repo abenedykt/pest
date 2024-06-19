fb51f1cb24b305f5c0a832d12446758bbcda0ccd
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-06-19 20:35:42 +0200

    working on skeleton

---
+++ tests/pest.performance.tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace pest.performance.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/pest.tests.e2e/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace pest.tests.e2e;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
