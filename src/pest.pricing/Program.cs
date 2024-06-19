67c27c5b51554f20fb7d9e5d560698369217dbd5
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-06-19 20:35:42 +0200

    injecting local ai with SemanticKernel

--- src/pest.pricing/Program.cs
+++ src/pest.pricing/Program.cs
@@ -5,21 +5,23 @@ using Codeblaze.SemanticKernel.Connectors.Ollama;
 
 #pragma warning disable SKEXP0010 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
-var kernelBuilder = Kernel.CreateBuilder();
+// var kernelBuilder = Kernel.CreateBuilder();
 
 var modelId = "phi3";
 var ollamaUri = new Uri("http://localhost:11434");
 
 // builder.Services.AddOpenAIChatCompletion(modelId, ollamaUri, apiKey: null);
-kernelBuilder.AddOllamaTextGeneration(modelId, ollamaUri);
+// kernelBuilder.AddOllamaTextGeneration(modelId, ollamaUri);
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // lets some AI Kernel
-builder.Services.AddKernel();
+var kernel = builder.Services.AddKernel();
+kernel.AddOpenAIChatCompletion(modelId, ollamaUri, apiKey: null);
 
-builder.Services.AddTransient<HttpClient>();
+
+// builder.Services.AddTransient<HttpClient>();
 
 
 
