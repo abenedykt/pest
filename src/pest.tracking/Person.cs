f8ac5d1adf8bb5bc96f9a0907dbf4e58bdf7caf2
Arkadiusz Benedykt <arkadiusz.benedykt@pb.com>
2024-06-26 17:48:51 +0200

    example test

---
+++ src/pest.tracking/Person.cs
@@ -0,0 +1,12 @@
+namespace pest.examples;
+
+public class Person{
+    public Person(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+
+    public string Name { get; }
+    public string Surname { get; }
+}
--- tests/pest.performance.tests/UnitTest1.cs
+++ tests/pest.performance.tests/UnitTest1.cs
@@ -1,3 +1,6 @@
+using pest.examples;
+
+
 namespace pest.performance.tests;
 
 public class UnitTest1
@@ -5,6 +8,8 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-
+        var x = new Person("Miguel", "Camba");
+        Assert.Equal("Miguel", x.Name);
+        Assert.Equal("Camba", x.Surname);
     }
 }
