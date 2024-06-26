cf1f5640e78b507a3ee11be87d71a253d4657e2f
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-06-26 21:04:00 +0200

    yet more tests XD XD XD

--- tests/pest.tests.e2e/Pricing_WeatherForecastTests.cs
+++ tests/pest.tests.e2e/Pricing_WeatherForecastTests.cs
@@ -1,6 +1,8 @@
+using pest.pricing;
+
 namespace pest.tests.e2e;
 
-public class WeatherForecastTests
+public class Pricing_WeatherForecastTests
 {
     [Fact]
     public void WeatherForecast_returns_date_temperature_and_summary()
@@ -11,5 +13,14 @@ public class WeatherForecastTests
         Assert.Equal(DateOnly.FromDateTime(DateTime.Now), x.Date);
         Assert.Equal(temp, x.TemperatureC);
         Assert.Equal(text, x.Summary);
+    }
+    
+    [Fact]
+    public void Given_temperature_in_celsius_it_returns_temperature_in_fahrenheit()
+    {
+        var temp = Random.Shared.Next(-20, 45);
+        var text = "Short weather description for " + temp + " degrees Celsius?";
+        var x = new WeatherForecast(DateOnly.FromDateTime(DateTime.Now), temp, text);
+        Assert.Equal(32 + (int)(temp / 0.5556), x.TemperatureF);
     }
 }
