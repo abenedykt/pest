4ae85366ece37eac4867ce84b0dfddebc01873b9
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-04 14:33:18 +0200

    stop run.sh -> stop executing commands if previous fails

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
+# stop executing script if any error occurs
+set -e
+
+# build and run the docker container
 docker compose build 
 docker compose up -d
 
+
+# open startup.html that contains links to all the services and tools
 open startup.html
