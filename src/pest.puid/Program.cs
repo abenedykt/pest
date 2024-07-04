aeceec98f21a0b4eb24fa13a133eaabe3e6108ad
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-04 14:34:30 +0200

    adding healthcheck to pest.puid

--- src/pest.puid/Program.cs
+++ src/pest.puid/Program.cs
@@ -1,14 +1,20 @@
+using HealthChecks.UI.Client;
 using IdGen;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IIdGenerator<long>>(new IdGenerator(0));
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 app.MapGet("/", (IIdGenerator<long> g) => g.CreateId()).WithOpenApi();
-
+app.MapHealthChecks("/health", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 app.Run();
     
 public partial class Program {
