d31ee43e75d2bd07ec481ceffd4f1c189f587c97
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-08 14:09:23 +0200

    outbox pattern - ef + npgsql

--- src/pest.parcel/Endpoints/IOutbox.cs
+++ src/pest.parcel/Endpoints/IOutbox.cs
@@ -9,23 +9,23 @@ public interface IOutbox
 
 public class Outbox : IOutbox
 {
-    private readonly DataContext _db;
+    private readonly OutboxDbContext _db;
 
-    public Outbox(DataContext dataContext)
-    {
-        _db = dataContext;
-    }
+    // public Outbox(DataContext dataContext)
+    // {
+    //     _db = dataContext;
+    // }
     public void Publish<T>(T message)
     {
         var content = JsonSerializer.Serialize(message);
-
-        _db.OutboxMessages.Add(new OutboxMessage
+        var _db = new OutboxDbContext();
+        _db.Messages.Add(new OutboxMessage
         {
-            Content = content,
-            CreatedAt = DateTime.UtcNow
+              Data = content,
+              CreatedAt = DateTime.UtcNow
         });
-
-        _db.SaveChanges();
+        //
+         _db.SaveChanges();
         // here we would publish the message to the message broker
         // for now, let's just print it to the console
         Console.WriteLine($"Message published: {content}");
--- src/pest.parcel/Program.cs
+++ src/pest.parcel/Program.cs
@@ -1,8 +1,7 @@
-using EFCore.AutomaticMigrations;
+
 using FluentValidation;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Pest.Parcel;
 using Pest.Parcel.Endpoints;
 using Pest.Parcel.Extenstions;
 
@@ -16,22 +15,23 @@ builder.Services.AddHealthChecks();
 builder.Services.AddMinimalEndpoints();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 builder.Services.AddTransient<IOutbox, Outbox>();
-builder.Services.AddSingleton<DataContext>();
+// builder.Services.AddSingleton<DataContext>();
 
 var app = builder.Build();
 
 
-await using AsyncServiceScope serviceScope = app.Services.CreateAsyncScope();
-await using DataContext? dataContext = serviceScope.ServiceProvider.GetService<DataContext>();
+// await using AsyncServiceScope serviceScope = app.Services.CreateAsyncScope();
+// await using DataContext? dataContext = serviceScope.ServiceProvider.GetService<DataContext>();
 
-if (dataContext is not null)
-{
-    await dataContext.MigrateToLatestVersionAsync(new DbMigrationsOptions
-    {
-        AutomaticMigrationDataLossAllowed = true, // <- this is dangerous in production and should be avoided
-        AutomaticMigrationsEnabled = true
-    });
-}
+// if (dataContext is not null)
+// {
+//     dataContext.MigrateToLatestVersion(new DbMigrationsOptions
+//     {
+//         AutomaticMigrationDataLossAllowed = true, // <- this is dangerous in production and should be avoided
+//         AutomaticMigrationsEnabled = true
+//     });
+// }
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
