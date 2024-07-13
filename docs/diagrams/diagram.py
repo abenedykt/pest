6dae9db29113cae0107382d53db1d4d965f15895
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-13 10:36:45 +0200

    docs: todo graph as a code

--- docs/diagrams/diagram.py
+++ docs/diagrams/diagram.py
@@ -1,8 +1,30 @@
-# diagram.py
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import ECS
+from diagrams.onprem.queue import Kafka
+from diagrams.onprem.network import Nginx
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+show_image = True
+
+with Diagram("Parcel service with outbox", show=show_image):
+
+    ingres =  Nginx("ingress")
+
+    with Cluster("parcel service"): 
+        parcel_service = ECS("parcel service") 
+        outbox_database = ECS("outbox database")
+        outbox_worker = ECS("outbox worker")
+    
+    with Cluster("puid service"):
+        puid_service = ECS("puid")
+
+    event_bus = Kafka("events")
+
+
+
+    ingres >> parcel_service
+    parcel_service << puid_service
+    parcel_service >> outbox_database
+    
+
+    outbox_worker << outbox_database
+    outbox_worker >> event_bus
