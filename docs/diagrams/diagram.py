2619ce904426f687de1ba336e5844e7b2dc2a295
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-14 10:51:34 +0200

    docs: todo graph as a code

--- docs/diagrams/diagram.py
+++ docs/diagrams/diagram.py
@@ -1,7 +1,8 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.compute import ECS
 from diagrams.onprem.queue import Kafka
 from diagrams.onprem.network import Nginx
+from diagrams.azure.database import DatabaseForPostgresqlServers
 
 show_image = True
 
@@ -9,22 +10,21 @@ with Diagram("Parcel service with outbox", show=show_image):
 
     ingres =  Nginx("ingress")
 
-    with Cluster("parcel service"): 
-        parcel_service = ECS("parcel service") 
-        outbox_database = ECS("outbox database")
-        outbox_worker = ECS("outbox worker")
-    
     with Cluster("puid service"):
         puid_service = ECS("puid")
+
+    with Cluster("parcel service"): 
+        parcel_service = ECS("parcel service") 
+        outbox_database = DatabaseForPostgresqlServers("outbox database")
+        outbox_worker = ECS("outbox worker")
 
     event_bus = Kafka("events")
 
 
-
     ingres >> parcel_service
-    parcel_service << puid_service
-    parcel_service >> outbox_database
+    parcel_service << Edge(label = "fetch puid") << puid_service
+    parcel_service >> Edge(label = "save outgoing letter") >> outbox_database
     
 
-    outbox_worker << outbox_database
-    outbox_worker >> event_bus
+    outbox_worker << Edge(label = "fetch outgoing letters") << outbox_database
+    outbox_worker >> Edge(label = "publish") >> event_bus
