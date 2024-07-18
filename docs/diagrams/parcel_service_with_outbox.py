41beb14ceb232c7c40457428b4d02e4f3a898918
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-18 09:57:58 +0200

    experiments with splines

--- docs/diagrams/parcel_service_with_outbox.py
+++ docs/diagrams/parcel_service_with_outbox.py
@@ -3,29 +3,35 @@ from diagrams.aws.compute import ECS
 from diagrams.onprem.queue import Kafka
 from diagrams.onprem.network import Nginx
 from diagrams.azure.database import DatabaseForPostgresqlServers
+from diagrams.c4 import SystemBoundary
+
 
 show_image = True
 
-with Diagram(show=show_image, direction="LR", filename="parcel_service_with_outbox", outformat="png"):
+graph_attr = {
+    "splines": "spline"
+}
 
-    ingres =  Nginx("ingress")
+with Diagram(show=show_image, direction="LR", filename="parcel_service_with_outbox", outformat="png" , graph_attr=graph_attr):
 
-
-    
-    with Cluster("Events"):
-        event_bus = Kafka("Kafka")     
-
-    with Cluster("external"):
+    with SystemBoundary("External"):
         puid_service = ECS("PUID Service")
+            
+    with SystemBoundary("Parcel Service"):
 
-    with Cluster("Parcel service", direction="LR"):         
-        parcel_service = ECS("REST API") 
-        parcel_service >> Edge(label = "/GET", style="dotted") >> puid_service
-        outbox = DatabaseForPostgresqlServers("Persisted Outbox")
-        parcel_service >> Edge(style="bold", label="to outbox") >> outbox
-        outbox_worker = ECS("Outbox Worker")
-        outbox << Edge(style="bold", label="process") << outbox_worker
+        ingres =  Nginx("ingress")
+    
+        with Cluster("Events"):
+            event_bus = Kafka("Kafka")     
 
-    outbox_worker >> Edge(label = "publish", style="bold") >> event_bus
+        with Cluster("Parcel service", direction="LR"):         
+            parcel_service = ECS("REST API") 
+            parcel_service >> Edge(label = "/GET", style="dotted") >> puid_service
+            outbox = DatabaseForPostgresqlServers("Persisted Outbox")
+            parcel_service >> Edge(style="bold", label="to outbox") >> outbox
+            outbox_worker = ECS("Outbox Worker")
+            outbox << Edge(style="bold", label="process") << outbox_worker
 
-    ingres >> parcel_service
+        outbox_worker >> Edge(label = "publish", style="bold") >> event_bus
+
+        ingres >> parcel_service
