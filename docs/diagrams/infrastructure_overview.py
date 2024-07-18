5b0db50de047737fe9d562b7322f2cce3443e912
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-18 12:23:25 +0200

    code reformat

--- docs/diagrams/infrastructure_overview.py
+++ docs/diagrams/infrastructure_overview.py
@@ -6,38 +6,37 @@ from diagrams.onprem.monitoring import Grafana
 from tools.DiagramTools import based_on_params
 
 show_image = based_on_params()
+
+
 def edge_persistency():
     return Edge(style="dotted")
 
 
 with Diagram(show=show_image, direction="TB", filename="infrastructure_overview", outformat="png"):
-
     with Cluster("persistency layer"):
-            parcel_persistency = ECS("Parcel Persistency")
-            pricing_persistency = ECS("Parcel Persistency")
-            tracing_persistency = ECS("Tracking Persistency")
+        parcel_persistency = ECS("Parcel Persistency")
+        pricing_persistency = ECS("Parcel Persistency")
+        tracing_persistency = ECS("Tracking Persistency")
 
     with Cluster("services"):
-            
         with Cluster("client services"):
             parcel_service = ECS("Parcel Service")
             parcel_service >> edge_persistency() >> parcel_persistency
-            
+
             pricing_service = ECS("Pricing Service")
             pricing_service >> edge_persistency() >> pricing_persistency
 
             tracking_service = ECS("Tracking Service")
             tracking_service >> edge_persistency() >> tracing_persistency
-            
+
         with Cluster("infrastructure services"):
-            scan_machines = [Integrations("machine 1"), Integrations("machine 2"),Integrations("machine n"),]
+            scan_machines = [Integrations("machine 1"), Integrations("machine 2"), Integrations("machine n"), ]
 
     with Cluster("infrastructure"):
         event_bus = Kafka("Kafka")
         monitoring = Grafana("Grafana")
 
-
-    pricing_service << Edge(label = "subscribe", style="bold") << event_bus
-    tracking_service << Edge(label = "subscribe", style="bold")  << event_bus
-    parcel_service >> Edge(label = "publish", style="bold")  >> event_bus
-    scan_machines >> Edge(label = "publish", style="bold") >> event_bus
+    pricing_service << Edge(label="subscribe", style="bold") << event_bus
+    tracking_service << Edge(label="subscribe", style="bold") << event_bus
+    parcel_service >> Edge(label="publish", style="bold") >> event_bus
+    scan_machines >> Edge(label="publish", style="bold") >> event_bus
--- docs/diagrams/parcel_service_with_outbox.py
+++ docs/diagrams/parcel_service_with_outbox.py
@@ -8,25 +8,22 @@ from tools.DiagramTools import based_on_params
 
 show_image = based_on_params()
 
-
 with Diagram(show=show_image, direction="LR", filename="parcel_service_with_outbox", outformat="png"):
-
     with SystemBoundary("External"):
         puid_service = ECS("PUID Service")
-        event_bus = Kafka("Kafka") 
+        event_bus = Kafka("Kafka")
 
-    ingres =  Nginx("ingress")
+    ingres = Nginx("ingress")
 
     with SystemBoundary("Parcel Service"):
-
-        with Cluster("Parcel service", direction="LR"):         
-            parcel_service = ECS("REST API") 
-            parcel_service >> Edge(label = "/GET", style="dotted") >> puid_service
+        with Cluster("Parcel service", direction="LR"):
+            parcel_service = ECS("REST API")
+            parcel_service >> Edge(label="/GET", style="dotted") >> puid_service
             outbox = DatabaseForPostgresqlServers("Persisted Outbox")
             parcel_service >> Edge(style="bold", label="to outbox") >> outbox
             outbox_worker = ECS("Outbox Worker")
             outbox << Edge(style="bold", label="process") << outbox_worker
 
-        outbox_worker >> Edge(label = "publish", style="bold") >> event_bus
+        outbox_worker >> Edge(label="publish", style="bold") >> event_bus
 
         ingres >> parcel_service
