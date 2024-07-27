8aaed873a0a3734c71e62b80c2b58d90f2b3b337
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-07-27 13:06:29 +0200

    finetuning start.sh - autorun docker on mac

--- run.sh
+++ run.sh
@@ -1,6 +1,25 @@
 # stop executing script if any error occurs
 set -e
 
+# start docker (macOS only)
+if [ "$(uname)" = "Darwin" ]; then
+  open -a Docker
+  
+  printf "Waiting for Docker to start."
+  timeout=60
+  start_time=$(date +%s)
+  while ! docker ps > /dev/null 2>&1; do
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    if [ $elapsed_time -gt $timeout ]; then
+      echo "Docker took too long to start. Bye!"
+      exit 1
+    fi
+      printf "."
+      sleep 1
+    done
+fi
+
 # build and run the docker container
 docker compose build 
 docker compose up -d
