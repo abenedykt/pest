e6a6f129faf5683cedcdfb59188720673db51bfa
Arkadiusz Benedykt <2996107+abenedykt@users.noreply.github.com>
2024-08-02 15:50:56 +0200

    refactor

--- src/pest.logging/PestLogging.cs
+++ src/pest.logging/PestLogging.cs
@@ -13,42 +13,42 @@ public static class PestLogging
 
     public static void AddLogging(this IHostApplicationBuilder builder)
     {
-        builder.Services.AddOpenTelemetry()
-            .WithTracing(tracing => tracing
-                // The rest of your setup code goes here
-                .AddOtlpExporter(options =>
-                {
-                    options.Endpoint = new Uri("http://seq/ingest/v1/traces");
-                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
-                }))
-            .WithMetrics(metrics => metrics
-                // The rest of your setup code goes here
-                .AddOtlpExporter(options =>
-                {
-                    options.Endpoint = new Uri("http://seq/ingest/v1/metrics");
-                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
-                }));
-
-        builder.Logging.AddOpenTelemetry(logging =>
-        {
-            // The rest of your setup code goes here
-            logging.AddOtlpExporter(options =>
-            {
-                options.Endpoint = new Uri("http://seq/ingest/v1/logs");
-                options.Protocol = OtlpExportProtocol.HttpProtobuf;
-            });
-        });
-        
-        // builder.Logging
-        //     .ClearProviders()
-        //     .AddOpenTelemetry(x =>
-        //     {
-        //         x.AddConsoleExporter();
-        //         x.AddOtlpExporter(c =>
+        // builder.Services.AddOpenTelemetry()
+        //     .WithTracing(tracing => tracing
+        //         // The rest of your setup code goes here
+        //         .AddOtlpExporter(options =>
         //         {
-        //             c.Endpoint = new Uri("http://seq/ingest/otlp/v1/logs");
-        //             c.Protocol = OtlpExportProtocol.HttpProtobuf;
-        //         });
-        //     });    
+        //             options.Endpoint = new Uri("http://seq/ingest/v1/traces");
+        //             options.Protocol = OtlpExportProtocol.HttpProtobuf;
+        //         }))
+        //     .WithMetrics(metrics => metrics
+        //         // The rest of your setup code goes here
+        //         .AddOtlpExporter(options =>
+        //         {
+        //             options.Endpoint = new Uri("http://seq/ingest/v1/metrics");
+        //             options.Protocol = OtlpExportProtocol.HttpProtobuf;
+        //         }));
+        //
+        // builder.Logging.AddOpenTelemetry(logging =>
+        // {
+        //     // The rest of your setup code goes here
+        //     logging.AddOtlpExporter(options =>
+        //     {
+        //         options.Endpoint = new Uri("http://seq/ingest/v1/logs");
+        //         options.Protocol = OtlpExportProtocol.HttpProtobuf;
+        //     });
+        // });
+        
+        builder.Logging
+            .ClearProviders()
+            .AddOpenTelemetry(x =>
+            {
+                x.AddConsoleExporter();
+                x.AddOtlpExporter(c =>
+                {
+                    c.Endpoint = new Uri("http://seq/ingest/otlp/v1/logs");
+                    c.Protocol = OtlpExportProtocol.HttpProtobuf;
+                });
+            });    
     }
 }
